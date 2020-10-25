cb191f63647beafedc6542a91b8616ef45a2a586
ginkuc <ginkuc@gmail.com>
2020-10-25 13:44:13 +0200

    Fixed naming and namespaces

--- Renamer/Program.cs
+++ Renamer/Program.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Renamer;
 
-namespace WinFormsCoreApp
+namespace Renamer
 {
     static class Program
     {
